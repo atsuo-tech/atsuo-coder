a4ec73153d9c39c92784f76f481cb0d6855795e5
Yama.can <yama.can95@gmail.com>
2024-02-13 20:47:02 +0900

    [update] Update who are showed in standings

--- front/src/app/(pages)/contests/[contest]/standings/page.tsx
+++ front/src/app/(pages)/contests/[contest]/standings/page.tsx
@@ -22,26 +22,61 @@ export default async function Page({ params: { contest: contestId } }: { params:
 
 	const [submissions, _] = await sql.query("SELECT * FROM submissions WHERE contest = ? ORDER BY created_at", [contestId]) as [{ id: string, sourceCode: string, contest: string, task: string, user: string, created_at: Date, judge: string, language: string }[], FieldPacket[]];
 
-	const scores: { [user: string]: { score: number, problems: { [problem: string]: number } } } = {};
+	const scores: { [user: string]: { score: number, problems: { [problem: string]: { score: number, penalty: number, notEffectedPenalty: number, lastSubmitTime: number } } } } = {};
 
 	for (let i = 0; i < submissions.length; i++) {
 
 		if (submissions[i].judge == "WJ" || JSON.parse(submissions[i].judge).status == 3) continue;
 		scores[submissions[i].user] = scores[submissions[i].user] || { score: 0, problems: {} };
-		scores[submissions[i].user].problems[submissions[i].task] = Math.max(scores[submissions[i].user].problems[submissions[i].task] || 0, JSON.parse(submissions[i].judge)[0][1]);
+
+		if (scores[submissions[i].user].problems[submissions[i].task] || -1 < JSON.parse(submissions[i].judge)[0][1]) {
+
+			scores[submissions[i].user].problems[submissions[i].task].penalty += scores[submissions[i].user].problems[submissions[i].task].notEffectedPenalty || 0;
+			scores[submissions[i].user].problems[submissions[i].task].lastSubmitTime = submissions[i].created_at.getTime() - (await contest.start!!.get()).getTime();
+			scores[submissions[i].user].problems[submissions[i].task].score = JSON.parse(submissions[i].judge)[0][1];
+			scores[submissions[i].user].problems[submissions[i].task].notEffectedPenalty = 1;
+
+		} else {
+
+			scores[submissions[i].user].problems[submissions[i].task].notEffectedPenalty = (scores[submissions[i].user].problems[submissions[i].task].notEffectedPenalty || 0) + 1;
+
+		}
 
 	}
 
-	const users: [number, string][] = [];
+	let users: { user: string, score: number, contestTime: number }[] = [];
 
 	for (const user in scores) {
+
+		let lastSubmitTime = 0;
+		let penalty = 0;
+
 		for (const problem in scores[user].problems) {
-			scores[user].score += scores[user].problems[problem];
+
+			scores[user].score += scores[user].problems[problem].score;
+			penalty += scores[user].problems[problem].penalty;
+			lastSubmitTime = Math.max(lastSubmitTime, scores[user].problems[problem].lastSubmitTime);
+
 		}
-		users.push([scores[user].score, user]);
+
+		users.push({ user, score: scores[user].score, contestTime: lastSubmitTime + (await contest.penalty!!.get()) * penalty });
+
 	}
 
-	users.sort((a, b) => b[0] - a[0]);
+	const rated_users = await contest.rated_users!!.get();
+	const unrated_users = await contest.unrated_users!!.get();
+
+	const registerd_users = rated_users.concat(unrated_users);
+
+	users = users.filter((user) => registerd_users.includes(user.user));
+
+	registerd_users.filter((user) => !users.find((value) => value.user == user)).map((user) => {
+
+		users.push({ user, score: 0, contestTime: 0 });
+
+	});
+
+	users.sort((a, b) => (b.score - a.score == 0 ? a.contestTime - b.contestTime : b.score - a.score));
 
 	return (
 		<>
@@ -71,12 +106,12 @@ export default async function Page({ params: { contest: contestId } }: { params:
 
 								nodes.push(
 									<tr key={i}>
-										<td className={styles.user}>{user[1]}</td>
-										<td className={styles.user}>{scores[user[1]].score}</td>
+										<td className={styles.user}>{user.user}</td>
+										<td className={styles.user}>{scores[user.user].score}</td>
 										{
 											(await contest.problems!!.get()).map((problem, j) => {
 												return (
-													<td key={j}>{scores[user[1]].problems[problem] || 0}</td>
+													<td key={j}>{scores[user.user].problems[problem].score || 0}</td>
 												)
 											})
 										}
