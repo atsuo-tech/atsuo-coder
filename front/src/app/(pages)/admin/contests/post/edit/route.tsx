96c13eeadf17c306c334183339949a1b03f83354
Yama.can <yama.can95@gmail.com>
2024-02-14 15:53:32 +0900

    [fix] Fix infinity settings

--- front/src/app/(pages)/admin/contests/post/edit/route.tsx
+++ front/src/app/(pages)/admin/contests/post/edit/route.tsx
@@ -56,7 +56,7 @@ export async function POST(req: NextRequest) {
 	await Promise.all([
 		contest.name!!.set(data.get("name") as string),
 		contest.start!!.set(new Date(data.get("start") as string)),
-		contest.period!!.set(Number(data.get("period")) * 1000 * 60),
+		contest.period!!.set(Number(data.get("period")) == -1 ? -1 : Number(data.get("period")) * 1000 * 60),
 		contest.problems!!.set((data.get("problems") as string).split(',').map((val) => val.replace(/[\s\t]/g, ""))),
 		contest.editors!!.set((data.get("editors") as string).split(',').map((val) => val.replace(/[\s\t]/g, ""))),
 		contest.testers!!.set((data.get("testers") as string).split(',').map((val) => val.replace(/[\s\t]/g, ""))),
