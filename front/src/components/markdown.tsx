1479567085318774f6a641e7a5aa3cd030b5e4c6
Yama.can <yama.can95@gmail.com>
2024-02-14 16:33:48 +0900

    [fix] Fix tex on markdown

--- front/src/components/markdown.tsx
+++ front/src/components/markdown.tsx
@@ -1,6 +1,8 @@
 import markdownHtml from 'zenn-markdown-html';
 import 'zenn-content-css';
 import './updater.css';
+import Head from 'next/head';
+import 'zenn-embed-elements';
 
 export default function Markdown({ md }: { md: string }) {
 
@@ -8,6 +10,9 @@ export default function Markdown({ md }: { md: string }) {
 
 	return (
 		<>
+			<Head>
+				<script src="https://embed.zenn.studio/js/listen-embed-event.js" async></script>
+			</Head>
 			<div
 				// "znc"というクラス名を指定する
 				className="znc"
