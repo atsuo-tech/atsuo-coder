d60b7883862764abafd283794ae5046da6f21c53
Yama.can <yama.can95@gmail.com>
2024-03-25 13:27:11 +0900

    [Add] Add cache setting page

---
+++ front/src/app/(pages)/admin/cache/post/contests/all/route.tsx
@@ -0,0 +1,17 @@
+import { hasAdminPremission } from "@/app/(pages)/admin/permission";
+import { clearCache } from "@/lib/contest";
+import { NextRequest } from "next/server";
+
+export async function POST(request: NextRequest) {
+
+	if (!(await hasAdminPremission())) {
+
+		return new Response("Forbidden", { status: 403 });
+
+	}
+
+	clearCache();
+
+	return new Response("OK");
+
+}
