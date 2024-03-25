4a67390f4fd139b65e06f80d345336fe9fe9cb68
Yama.can <yama.can95@gmail.com>
2024-03-25 13:45:16 +0900

    [add] Add timer to /tasks/layout.tsx

---
+++ front/src/app/(pages)/contests/[contest]/tasks/waiter.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default async function Waiter({ start }: { start: Date }) {
+
+	useEffect(() => {
+
+		setInterval(() => {
+
+			const under = new Date(start.getTime() - Date.now());
+
+			document.querySelector("#under")!!.textContent = `${under.getUTCDate() - 1}日 ${under.getUTCHours()}時間 ${under.getUTCMinutes()}分 ${under.getUTCSeconds()}秒`;
+
+			if (start.getTime() - Date.now() <= 0) {
+
+				location.reload();
+
+			}
+
+		}, 15);
+
+	});
+
+	return (
+		<>
+
+			<p id="under"></p>
+
+		</>
+	)
+
+}
